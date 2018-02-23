d9b22d7c100b67305e78094fea3570f34c1922da
Matthías Páll Gissurarson <mpg@mpg.is>
2018-02-23 12:19:15 +0000

    Added free monad test

---
+++ testsuite/tests/typecheck/should_compile/free_monad_substitutions.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -frefinement-level-substitutions=2 #-}
+module TG where
+import Prelude (Monad, Applicative, Functor, fmap, (<*>), pure, (>>=), (=<<), ($), (<$>))
+
+data Free f a = Pure a | Free (f (Free f a))
+
+instance Functor f => Functor (Free f) where
+    fmap f = go where
+      go (Pure a) =  Pure (f a)
+      go (Free fa) = Free (_a go fa)
+
+instance Functor f => Applicative (Free f) where
+  pure = Pure
+  Pure a <*> Pure b = Pure (a b)
+  Pure a <*> Free mb = Free (fmap a <$> mb)
+  Free ma <*> b = Free ((<*> b) <$> ma)
+
+instance Applicative f => Monad (Free f) where
+    Pure a >>= f = f a
+    Free f >>= g = Free (fmap _a f)
+  
