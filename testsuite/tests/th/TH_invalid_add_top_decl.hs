d81a5d8c382ab88413ef42e4becf922511ba62d0
Facundo Domínguez <facundo.dominguez@tweag.io>
2020-06-08 16:03:02 +0000

    Fix call of doE in test.

--- testsuite/tests/th/TH_invalid_add_top_decl.hs
+++ testsuite/tests/th/TH_invalid_add_top_decl.hs
@@ -3,6 +3,6 @@ import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
 $(do
-  invalidDecl <- valD (varP (mkName "emptyDo")) (normalB (doE Nothing [])) []
+  invalidDecl <- valD (varP (mkName "emptyDo")) (normalB (doE [])) []
   addTopDecls [invalidDecl]
   return [])
